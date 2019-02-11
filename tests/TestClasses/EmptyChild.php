e33b53d0862176596ed2620019fb02deb0c0f5b6
Tony Barry <tonympbarry@gmail.com>
2019-02-11 10:10:59 -0800

    Fixes #47 - Allowing empty DTO's to be cast via empty arrays

---
+++ tests/TestClasses/EmptyChild.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spatie\DataTransferObject\Tests\TestClasses;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class EmptyChild extends DataTransferObject
+{
+}
