8d355e9aab80fc180c1384bef8af0c8cdde39bd2
Brent Roose <brent@spatie.be>
2019-03-15 14:03:32 +0100

    Improve tests

--- tests/ImmutableTest.php
+++ tests/ImmutableTest.php
@@ -8,7 +8,7 @@ use Spatie\DataTransferObject\Tests\TestClasses\TestDataTransferObject;
 class ImmutableTest extends TestCase
 {
     /** @test */
-    public function it()
+    public function immutable_values_cannot_be_overwritten()
     {
         $dto = TestDataTransferObject::immutable([
             'testProperty' => 1
@@ -19,5 +19,15 @@ class ImmutableTest extends TestCase
         $this->expectException(DataTransferObjectError::class);
 
         $dto->testProperty = 2;
+    }
+
+    /** @test */
+    public function method_calls_are_proxied()
+    {
+        $dto = TestDataTransferObject::immutable([
+            'testProperty' => 1
+        ]);
+
+        $this->assertEquals(['testProperty' => 1], $dto->toArray());
     }
 }
