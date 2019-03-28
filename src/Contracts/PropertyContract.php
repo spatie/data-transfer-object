a151495a7bfca20403ca274d5ec548302b5b4a67
deviouspk <aamining2@gmail.com>
2019-03-28 15:42:54 +0100

    remove author notes

--- src/Contracts/PropertyContract.php
+++ src/Contracts/PropertyContract.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tony
- * Date: 28.03.19
- * Time: 15:12
- */
 
 namespace Spatie\DataTransferObject\Contracts;
 
--- src/Exceptions/ImmutablePropertyDtoException.php
+++ src/Exceptions/ImmutablePropertyDtoException.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tony
- * Date: 28.03.19
- * Time: 15:02
- */
 
 namespace Spatie\DataTransferObject\Exceptions;
 
