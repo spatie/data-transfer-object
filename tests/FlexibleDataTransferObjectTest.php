c9667182abb048ac53ae89ef603df83e81c8c3c8
brendt <brendt@users.noreply.github.com>
2019-12-13 12:21:00 +0000

    Fix styling

--- tests/FlexibleDataTransferObjectTest.php
+++ tests/FlexibleDataTransferObjectTest.php
@@ -9,7 +9,8 @@ class FlexibleDataTransferObjectTest extends TestCase
     /** @test */
     public function flexible_dto_can_be_given_unknown_properties()
     {
-        $dto = new class(['unknown' => 'test']) extends FlexibleDataTransferObject {};
+        $dto = new class(['unknown' => 'test']) extends FlexibleDataTransferObject {
+        };
 
         $this->markTestSucceeded();
     }
