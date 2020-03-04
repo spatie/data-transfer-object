4eabdc4f6fca59b8a46bf53c1204e7b58c2c2ccd
Scott Carpenter <scott@payforstay.com>
2020-03-04 14:51:38 -0700

    Add helpful properties to thrown exceptions and expose them through public getters

--- src/DataTransferObjectError.php
+++ src/DataTransferObjectError.php
@@ -8,11 +8,23 @@ use TypeError;
 
 class DataTransferObjectError extends TypeError
 {
+    use HasErrorProperties;
+
+    public function __construct($message, $properties = [])
+    {
+        parent::__construct($message);
+        $this->setProperties($properties);
+    }
+
     public static function unknownProperties(array $properties, string $className): DataTransferObjectError
     {
         $propertyNames = implode('`, `', $properties);
 
-        return new self("Public properties `{$propertyNames}` not found on {$className}");
+        return new self("Public properties `{$propertyNames}` not found on {$className}", [
+            'error'      => __FUNCTION__,
+            'class'      => $className,
+            'properties' => $properties,
+        ]);
     }
 
     public static function invalidType(
@@ -35,18 +47,32 @@ class DataTransferObjectError extends TypeError
             $value = 'array';
         }
 
-        $expectedTypes = implode(', ', $expectedTypes);
+        $expectedTypesList = implode(', ', $expectedTypes);
 
-        return new self("Invalid type: expected `{$class}::{$field}` to be of type `{$expectedTypes}`, instead got value `{$value}`, which is {$currentType}.");
+        return new self("Invalid type: expected `{$class}::{$field}` to be of type `{$expectedTypesList}`, instead got value `{$value}`, which is {$currentType}.", [
+            'error'         => __FUNCTION__,
+            'class'         => $class,
+            'property'      => $field,
+            'value'         => $value,
+            'type'          => $currentType,
+            'expectedTypes' => $expectedTypes,
+        ]);
     }
 
     public static function uninitialized(string $class, string $field): DataTransferObjectError
     {
-        return new self("Non-nullable property `{$class}::{$field}` has not been initialized.");
+        return new self("Non-nullable property `{$class}::{$field}` has not been initialized.", [
+            'error'    => __FUNCTION__,
+            'class'    => $class,
+            'property' => $field,
+        ]);
     }
 
     public static function immutable(string $property): DataTransferObjectError
     {
-        return new self("Cannot change the value of property {$property} on an immutable data transfer object");
+        return new self("Cannot change the value of property {$property} on an immutable data transfer object", [
+            'error'    => __FUNCTION__,
+            'property' => $property,
+        ]);
     }
 }
---
+++ src/HasErrorProperties.php
@@ -0,0 +1,128 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spatie\DataTransferObject;
+
+trait HasErrorProperties
+{
+    /** @var string */
+    protected $error = '';
+
+    /** @var string */
+    protected $class = '';
+
+    /** @var string */
+    protected $property = '';
+
+    /** @var array */
+    protected $properties = [];
+
+    /** @var string */
+    protected $value = '';
+
+    /** @var string */
+    protected $type = '';
+
+    /** @var array */
+    protected $expectedTypes = [];
+
+    /**
+     * Set the error properties
+     *
+     * @param array $properties
+     */
+    protected function setProperties(array $properties): void
+    {
+        foreach ($properties as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    /**
+     * Get the type of error being thrown
+     * The error type corresponds to the name of the function that generated it
+     *
+     * Error types: unknownProperties, invalidType, uninitialized, immutable
+     *
+     * @return string
+     */
+    public function getError(): string
+    {
+        return $this->error;
+    }
+
+    /**
+     * Get the name of the data transfer object class
+     *
+     * Applies to error types: unknownProperties, invalidType, uninitialized, immutable
+     *
+     * @return string
+     */
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    /**
+     * Get the name of the data transfer object property
+     *
+     * Applies to error types: invalidType, uninitialized, immutable
+     *
+     * @return string
+     */
+    public function getProperty(): string
+    {
+        return $this->property;
+    }
+
+    /**
+     * Get the names of the data transfer object properties
+     *
+     * Applies to error types: unknownProperties
+     *
+     * @return array
+     */
+    public function getProperties(): array
+    {
+        return $this->properties;
+    }
+
+    /**
+     * Get the value of the data transfer object property
+     *
+     * Applies to error types: invalidType
+     *
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * Get the type of the data transfer object property
+     *
+     * Applies to error types: invalidType
+     *
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Get the expected types of the data transfer object property
+     *
+     * Applies to error types: invalidType
+     *
+     * @return array
+     */
+    public function getExpectedTypes(): array
+    {
+        return $this->expectedTypes;
+    }
+}
