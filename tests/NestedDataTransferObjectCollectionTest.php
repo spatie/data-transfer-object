d03ada49833c29097e8064826511bcc37f6a724f
Craig Morris <craig.michael.morris@gmail.com>
2020-05-31 19:38:52 +0100

    Add more tests

--- tests/NestedDataTransferObjectCollectionTest.php
+++ tests/NestedDataTransferObjectCollectionTest.php
@@ -20,15 +20,52 @@ class NestedDataTransferObjectCollectionTest extends TestCase
             ],
         ]);
 
-        $this->assertEquals('test', $a->bs[0]->name);
-        $this->assertEquals('test2', $a->bs[2]->name);
-        $this->assertEquals('test3', $a->bs[3]->name);
+        $this->assertEquals('test', $a->children[0]->name);
+        $this->assertEquals('test2', $a->children[1]->name);
+        $this->assertEquals('test3', $a->children[2]->name);
+    }
+
+    /** @test */
+    public function with_parent_docblock_property()
+    {
+        $this->markTestSkipped('@todo');
+
+        $a = new DtoParentPropertyTypeUsingDocblock([
+            'children' => [
+                ['name' => 'test'],
+                ['name' => 'test2'],
+                ['name' => 'test3'],
+            ],
+        ]);
+
+        $this->assertEquals('test', $a->children[0]->name);
+        $this->assertEquals('test2', $a->children[1]->name);
+        $this->assertEquals('test3', $a->children[2]->name);
+    }
+
+    /** @test */
+    public function with_collection_docblock()
+    {
+        $this->markTestSkipped('@todo');
+
+        $a = new DtoParentPropertyTypeWithCollectionDocblock([
+            'children' => [
+                ['name' => 'test'],
+                ['name' => 'test2'],
+                ['name' => 'test3'],
+            ],
+        ]);
+
+        $this->assertEquals('test', $a->children[0]->name);
+        $this->assertEquals('test2', $a->children[1]->name);
+        $this->assertEquals('test3', $a->children[2]->name);
     }
 }
 
+// Scenario 1
 class DtoParentPropertyType extends DataTransferObject
 {
-    public DtoChildCollectionType $children;
+    public DtoChildPropertyTypeCollection $children;
 }
 
 class DtoChildPropertyType extends DataTransferObject
@@ -36,10 +73,30 @@ class DtoChildPropertyType extends DataTransferObject
     public string $name;
 }
 
-class DtoChildCollectionType extends DataTransferObjectCollection
+class DtoChildPropertyTypeCollection extends DataTransferObjectCollection
 {
     public function current(): DtoChildPropertyType
     {
         return parent::current();
     }
+}
+
+// Scenario 2
+class DtoParentPropertyTypeUsingDocblock extends DataTransferObject
+{
+    /** @var DtoChildPropertyTypeCollection */
+    public $children;
+}
+
+// Scenario 3
+class DtoParentPropertyTypeWithCollectionDocblock extends DataTransferObject
+{
+    public DtoChildPropertyTypeCollectionWithDocblock $children;
+}
+
+/**
+ * @method PostData current
+ */
+class DtoChildPropertyTypeCollectionWithDocblock extends DataTransferObjectCollection
+{
 }
