46139d8d920974ba9a34c4b5c32ea850f47a6234
Markus Podar <markus@fischer.name>
2020-08-04 10:59:31 +0200

    Collect invalid type errors instead of stopping at the first
    I deal with a lot of bigger DTOs, lots of properties. Invalid payloads
    happen all the time but it's annoying to only always get the first
    invalid type reported when they're more to wait.
    
    This PR will not change single invalid type reporting but will support
    multiple lines:
    ```
    Spatie\DataTransferObject\DataTransferObjectError : The following invalid types were encountered:
    expected `App\Services\DTO\Article::title` to be of type `string`, instead got value `null`, which is NULL
    expected `App\Services\DTO\Article::created_at` to be of type `Carbon\CarbonImmutable`, instead got value `null`, which is NULL
    
    ```

--- src/DataTransferObjectError.php
+++ src/DataTransferObjectError.php
@@ -15,12 +15,21 @@ class DataTransferObjectError extends TypeError
         return new self("Public properties `{$propertyNames}` not found on {$className}");
     }
 
-    public static function invalidType(
+    public static function invalidTypes(array $invalidTypes): DataTransferObjectError
+    {
+        $msg = count($invalidTypes) > 1
+            ? "The following invalid types were encountered:\n" . implode("\n", $invalidTypes) . "\n"
+            : "Invalid type: {$invalidTypes[0]}.";
+
+        throw new self($msg);
+    }
+
+    public static function invalidTypeMessage(
         string $class,
         string $field,
         array $expectedTypes,
         $value
-    ): DataTransferObjectError {
+    ): string {
         $currentType = gettype($value);
 
         if ($value === null) {
@@ -37,7 +46,7 @@ class DataTransferObjectError extends TypeError
 
         $expectedTypes = implode(', ', $expectedTypes);
 
-        return new self("Invalid type: expected `{$class}::{$field}` to be of type `{$expectedTypes}`, instead got value `{$value}`, which is {$currentType}.");
+        return "expected `{$class}::{$field}` to be of type `{$expectedTypes}`, instead got value `{$value}`, which is {$currentType}";
     }
 
     public static function uninitialized(string $class, string $field): DataTransferObjectError
