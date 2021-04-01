e43870f354d7eda65ecabdc80ee32374de515145
Brent Roose <brent@spatie.be>
2021-04-01 06:45:48 +0200

    Code cleanup

--- tests/ScalarPropertyTest.php
+++ tests/ScalarPropertyTest.php
@@ -9,10 +9,13 @@ class ScalarPropertyTest extends TestCase
     /** @test */
     public function scalar_property_can_be_set()
     {
-        $dto = new class(foo: 1) extends DataTransferObject {
-            public int $foo;
-        };
+        $dto = new ScalarPropertyDto(foo: 1);
 
         $this->assertEquals(1, $dto->foo);
     }
+}
+
+class ScalarPropertyDto extends DataTransferObject
+{
+    public int $foo;
 }
