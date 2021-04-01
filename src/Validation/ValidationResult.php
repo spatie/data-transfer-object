8ebe551d1029b6211f30a83123039c3aa1ded3c8
Brent Roose <brent@spatie.be>
2021-04-01 06:36:52 +0200

    Add static constructors for ValidationResult

--- src/Validation/ValidationResult.php
+++ src/Validation/ValidationResult.php
@@ -13,4 +13,19 @@ class ValidationResult
         public ?string $message = null
     ) {
     }
+
+    public static function valid(): self
+    {
+        return new self(
+            isValid: true,
+        );
+    }
+
+    public static function invalid(string $message): self
+    {
+        return new self(
+            isValid: false,
+            message: $message,
+        );
+    }
 }
