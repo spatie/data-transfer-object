e258aff07e7f9e099bee8cfc6d3a847099301d05
M_A_X_NSK <hardstyle@myttk.ru>
2022-06-02 20:26:56 +0700

    Getting rid of extra type checking

--- src/Casters/EnumCaster.php
+++ src/Casters/EnumCaster.php
@@ -2,24 +2,18 @@
 
 namespace Spatie\DataTransferObject\Casters;
 
-use LogicException;
 use Spatie\DataTransferObject\Caster;
 
 class EnumCaster implements Caster
 {
     public function __construct(
         private array $types,
-        private string $enumType,
-        private string $enumValueType
+        private string $enumType
     ) {
     }
 
     public function cast(mixed $value): mixed
     {
-        if (gettype($value) !== $this->enumValueType) {
-            throw new LogicException("$this->enumType can only be casted from $this->enumValueType");
-        }
-
         return $this->enumType::from($value);
     }
 }
