05eef7b74864466d80c0fb2a2bbe347c3caff718
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-28 21:25:49 +0300

    refactor

--- tests/CollectionCaster/CollectionCasterTest.php
+++ tests/CollectionCaster/CollectionCasterTest.php
@@ -6,52 +6,51 @@ use Illuminate\Support\Collection;
 use Spatie\DataTransferObject\Attributes\CastWith;
 use Spatie\DataTransferObject\Caster;
 use Spatie\DataTransferObject\DataTransferObject;
-use Spatie\DataTransferObject\Tests\TestCase;
+use function PHPUnit\Framework\assertCount;
+use function PHPUnit\Framework\assertInstanceOf;
 
-class CollectionCasterTest extends TestCase
-{
-    /** @test */
-    public function test_collection_caster()
+beforeAll(function () {
+    class Bar extends DataTransferObject
     {
-        $bar = new Bar([
-            'collectionOfFoo' => [
-                ['name' => 'a'],
-                ['name' => 'b'],
-                ['name' => 'c'],
-            ],
-        ]);
-
-        $this->assertCount(3, $bar->collectionOfFoo);
-        $this->assertInstanceOf(Foo::class, $bar->collectionOfFoo[0]);
+        #[CastWith(FooCollectionCaster::class)]
+        public CollectionOfFoo $collectionOfFoo;
     }
-}
 
-class Bar extends DataTransferObject
-{
-    #[CastWith(FooCollectionCaster::class)]
-    public CollectionOfFoo $collectionOfFoo;
-}
-
-class Foo extends DataTransferObject
-{
-    public string $name;
-}
-
-class CollectionOfFoo extends Collection
-{
-    public function offsetGet($key): Foo
+    class Foo extends DataTransferObject
     {
-        return parent::offsetGet($key);
+        public string $name;
     }
-}
 
-class FooCollectionCaster implements Caster
-{
-    public function cast(mixed $value): CollectionOfFoo
+    class CollectionOfFoo extends Collection
     {
-        return new CollectionOfFoo(array_map(
-            fn (array $data) => new Foo(...$data),
-            $value
-        ));
+        public function offsetGet($key): Foo
+        {
+            return parent::offsetGet($key);
+        }
     }
-}
+
+    class FooCollectionCaster implements Caster
+    {
+        public function cast(mixed $value): CollectionOfFoo
+        {
+            return new CollectionOfFoo(array_map(
+                fn (array $data) => new Foo(...$data),
+                $value
+            ));
+        }
+    }
+
+});
+
+test('collection caster', function () {
+    $bar = new Bar([
+        'collectionOfFoo' => [
+            ['name' => 'a'],
+            ['name' => 'b'],
+            ['name' => 'c'],
+        ],
+    ]);
+
+    assertCount(3, $bar->collectionOfFoo);
+    assertInstanceOf(Foo::class, $bar->collectionOfFoo[0]);
+});
