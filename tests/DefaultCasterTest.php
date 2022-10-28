6e9832ccb8630a7493c9258f06f7b6cb7ad8d68c
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-28 21:56:51 +0300

    refactor

--- tests/DefaultCasterTest.php
+++ tests/DefaultCasterTest.php
@@ -8,51 +8,47 @@ use Spatie\DataTransferObject\Attributes\DefaultCast;
 use Spatie\DataTransferObject\Caster;
 use Spatie\DataTransferObject\DataTransferObject;
 
-class DefaultCasterTest extends TestCase
-{
-    /** @test */
-    public function property_is_casted()
-    {
-        $dto = new DtoWithDefaultCaster(date: '2020-01-01');
+beforeAll(function () {
 
-        $this->markTestSucceeded();
+    #[DefaultCast(DateTimeImmutable::class, DateTimeImmutableCaster::class)]
+    class DtoWithDefaultCaster extends DataTransferObject
+    {
+        public DateTimeImmutable $date;
     }
 
-    /** @test */
-    public function child_property_is_casted()
+    #[DefaultCast(DateTimeImmutable::class, DateTimeImmutableCaster::class)]
+    abstract class AbstractWithDefaultCaster extends DataTransferObject
     {
-        $dto = new ChildDto(date: '2020-01-01');
-
-        $this->markTestSucceeded();
     }
-}
 
-#[DefaultCast(DateTimeImmutable::class, DateTimeImmutableCaster::class)]
-class DtoWithDefaultCaster extends DataTransferObject
-{
-    public DateTimeImmutable $date;
-}
-
-#[DefaultCast(DateTimeImmutable::class, DateTimeImmutableCaster::class)]
-abstract class AbstractWithDefaultCaster extends DataTransferObject
-{
-}
-
-class ChildDto extends AbstractWithDefaultCaster
-{
-    public DateTimeImmutable $date;
-}
-
-#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
-class DateTimeImmutableCaster implements Caster
-{
-    /**
-     * @param string|mixed $value
-     *
-     * @return DateTimeImmutable
-     */
-    public function cast(mixed $value): DateTimeImmutable
+    class ChildDto extends AbstractWithDefaultCaster
     {
-        return DateTimeImmutable::createFromFormat('Y-m-d', $value);
+        public DateTimeImmutable $date;
     }
-}
+
+    #[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD)]
+    class DateTimeImmutableCaster implements Caster
+    {
+        /**
+         * @param string|mixed $value
+         *
+         * @return DateTimeImmutable
+         */
+        public function cast(mixed $value): DateTimeImmutable
+        {
+            return DateTimeImmutable::createFromFormat('Y-m-d', $value);
+        }
+    }
+});
+
+test('property is casted', function () {
+    $dto = new DtoWithDefaultCaster(date: '2020-01-01');
+
+    $this->markTestSucceeded();
+});
+
+test('child property is casted', function () {
+    $dto = new ChildDto(date: '2020-01-01');
+
+    $this->markTestSucceeded();
+});
