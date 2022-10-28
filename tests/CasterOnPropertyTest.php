4d3db8e9c93ab0752bbdf9fff1d0aef4c919841b
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-28 21:38:00 +0300

    refactor

--- tests/CasterOnPropertyTest.php
+++ tests/CasterOnPropertyTest.php
@@ -6,17 +6,13 @@ use Spatie\DataTransferObject\Attributes\CastWith;
 use Spatie\DataTransferObject\DataTransferObject;
 use Spatie\DataTransferObject\Tests\Dummy\ComplexObject;
 use Spatie\DataTransferObject\Tests\Dummy\ComplexObjectCaster;
+use function PHPUnit\Framework\assertEquals;
 
-class CasterOnPropertyTest extends TestCase
-{
-    /** @test */
-    public function property_is_casted()
-    {
-        $dto = new class (complexObject: [ 'name' => 'test' ]) extends DataTransferObject {
-            #[CastWith(ComplexObjectCaster::class)]
-            public ComplexObject $complexObject;
-        };
+test('property is casted', function () {
+    $dto = new class (complexObject: [ 'name' => 'test' ]) extends DataTransferObject {
+        #[CastWith(ComplexObjectCaster::class)]
+        public ComplexObject $complexObject;
+    };
 
-        $this->assertEquals('test', $dto->complexObject->name);
-    }
-}
+    assertEquals('test', $dto->complexObject->name);
+});
