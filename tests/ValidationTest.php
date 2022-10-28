d0416eadc23469e64407238ba3c3541ffda759db
Ayoob Mohammed <ayoob.sub@gmail.com>
2022-10-28 22:16:19 +0300

    refactor

--- tests/ValidationTest.php
+++ tests/ValidationTest.php
@@ -5,24 +5,18 @@ namespace Spatie\DataTransferObject\Tests;
 use Spatie\DataTransferObject\DataTransferObject;
 use Spatie\DataTransferObject\Exceptions\ValidationException;
 use Spatie\DataTransferObject\Tests\Dummy\NumberBetween;
+use function PHPUnit\Framework\assertEquals;
 
-class ValidationTest extends TestCase
-{
-    /** @test */
-    public function test_validation()
-    {
-        $dto = new class (foo: 50) extends DataTransferObject {
-            #[NumberBetween(1, 100)]
-            public int $foo;
-        };
+test('validation', function () {
+    $dto = new class (foo: 50) extends DataTransferObject {
+        #[NumberBetween(1, 100)]
+        public int $foo;
+    };
 
-        $this->assertEquals(50, $dto->foo);
+    assertEquals(50, $dto->foo);
 
-        $this->expectException(ValidationException::class);
-
-        new class (foo: 150) extends DataTransferObject {
-            #[NumberBetween(1, 100)]
-            public int $foo;
-        };
-    }
-}
+    new class (foo: 150) extends DataTransferObject {
+        #[NumberBetween(1, 100)]
+        public int $foo;
+    };
+})->throws(ValidationException::class);
